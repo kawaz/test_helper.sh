1f2a09c83fb4b2ea265d9ce2ef6dfbf92418f442
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-02-02 20:04:26 +0900

    Support bash version is less than 4.3

--- helper.bash
+++ helper.bash
@@ -3,8 +3,13 @@
 equals_var() {
   declare -p $1 >/dev/null 2>&1 || return 1
   declare -p $2 >/dev/null 2>&1 || return 1
-  declare -n equals_var__left=$1
-  declare -n equals_var__right=$2
+  if true || [[ ${BASH_VERSINFO[0]} -lt 4 ]] || [[ ${BASH_VERSINFO[0]} == 4 && ${BASH_VERSINFO[1]} -lt 3 ]]; then
+    eval "$(declare -p "$1" | perl -pe's/\S+?=/equals_var__left=/')"
+    eval "$(declare -p "$2" | perl -pe's/\S+?=/equals_var__right=/')"
+  else
+    declare -n equals_var__left=$1
+    declare -n equals_var__right=$2
+  fi
   # 非配列
   if [[ $(declare -p $1 $2 | grep -E '^declare -[^ ]*[aA]' | wc -l) == 0 ]]; then
     [[ $equals_var__left == $equals_var__right ]]; return $?
